48208d93506b55342ccbbff59c1636001d623b3b
bentoBAUX <bennettpoh@gmail.com>
2025-03-25 21:34:16 +0100

    Added comments for Steep Parallax

--- Assets/Shaders/Parallax-Mapping.hlsl
+++ Assets/Shaders/Parallax-Mapping.hlsl
@@ -15,29 +15,45 @@ float2 ParallaxMapping(sampler2D depthMap, float2 texCoords, float3 viewDir, flo
 
 float2 SteepParallaxMapping(sampler2D depthMap, float2 texCoords, float3 viewDir, int numLayers, float depthScale)
 {
+    // Calculate the size of a single layer
     float layerDepth = 1.0 / numLayers;
 
+    // Determine the step vector based on our view angle
     float2 p = viewDir.xy * depthScale;
     float2 stepVector = p / numLayers;
 
+    // Initialise and set starting values before the loop
     float currentLayerDepth = 0.0;
     float2 currentTexCoords = texCoords;
+
+    // Sample with fixed LOD to avoid issues in loops that will cause GPU timeouts
     float currentDepthMapValue = tex2Dlod(depthMap, float4(currentTexCoords, 0, 0.0)).r;
 
+    // Loop until we have stepped too far below the surface (the surface is where the depth map says it is)
     while (currentLayerDepth < currentDepthMapValue)
     {
+        // Shift the texture coordinates in the direction of step vector
         currentTexCoords -= stepVector;
+
+        // Sample the depthmap with the new texture coordinate to get the new depth value
         currentDepthMapValue = tex2Dlod(depthMap, float4(currentTexCoords, 0, 0.0)).r;
+
+        // Move on to the next layer
         currentLayerDepth += layerDepth;
     }
 
+    // Compute the texture coordinates from the previous iteration
     float2 prevTexCoords = currentTexCoords + stepVector;
-    float afterDepth = currentDepthMapValue - currentLayerDepth;
-    float beforeDepth = tex2Dlod(depthMap, float4(prevTexCoords, 0, 0)).r - currentLayerDepth + layerDepth;
 
-    float weight = afterDepth / (afterDepth - beforeDepth);
+    // Compute how far below the surface we are at the current step
+    float surfaceOffsetAfter = currentDepthMapValue - currentLayerDepth;
+
+    // Compute how far above the surface we were at the previous step
+    float surfaceOffsetBefore = tex2Dlod(depthMap, float4(prevTexCoords, 0, 0)).r - currentLayerDepth + layerDepth;
+
+    // Linearly interpolate the texture coordinates using the calculated weight
+    float weight = surfaceOffsetAfter / (surfaceOffsetAfter - surfaceOffsetBefore);
     float2 finalTexCoords = prevTexCoords * weight + currentTexCoords * (1.0 - weight);
-
 
     return finalTexCoords;
 }
